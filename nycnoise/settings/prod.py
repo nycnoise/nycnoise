55c22cbc7b4abd864642c0ecef808cbad6fa6199
gregsadetsky <lepetitg@gmail.com>
2025-08-09 11:42:21 -0400

    .

--- nycnoise/settings/prod.py
+++ nycnoise/settings/prod.py
@@ -8,3 +8,14 @@ DEBUG = False
 LOGGING["handlers"]["console"] = {
     "class": "logging.StreamHandler",
 }
+
+# add a custom TEST.NAME to the prod database
+# as the current one, test_nycnoise, extremely annoyingly,
+# is not deletable as another session is using it.
+# I'm very annoyed at this solution, and at the fact that I can't get
+# to delete that database (and also don't know how it happened/how to avoid
+# this from happening again). this is of course a temporary patch...
+
+DATABASES["default"]["TEST"] = {
+    "NAME": "test_nycnoise_prod",
+}
